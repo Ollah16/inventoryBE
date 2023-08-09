f0a12e24aeb7638f8de1d188626688d0aeff00ff
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-08-09 17:28:14 -0400

    corserror

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 if (process.env.Node_ENV != "production") require("dotenv").config()
 const express = require("express")
 const app = express()
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://inventoryapp-5900c.web.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 const cors = require("cors")
 app.use(cors({ origin: 'https://inventoryapp-5900c.web.app' }))
 app.use(express.urlencoded({ extended: true }))
