167c91bcf48e148170bd99ef9c001274efa81f8e
Ollah16 <olaoluwaboluwatife@yahoo.com>
2023-08-21 09:17:35 -0400

    newCorrection3

--- controller/userControl.js
+++ controller/userControl.js
@@ -71,8 +71,7 @@ const handle_CartItem = async (req, res) => {
                 : item
             );
 
-            updateCart.filter(item => item.customerQuantity != 0)
-
+            updateCart = updateCart.filter(item => item.customerQuantity !== 0)
             await User.findByIdAndUpdate(id, { cart: updateCart });
         } catch (err) {
             console.log(err);
